803a5f8b949e1d6c37a4938a71b061dcd51d1741
Marco Gomes <marco6_1@hotmail.com>
2024-05-24 17:01:17 +0100

    watch list

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,29 @@
+class BookmarksController < ApplicationController
+  # before_action :set_list, only: %i[new create]
+  before_action :set_list, only: %i[new create]
+
+  def new
+    # We need @list in our `simple_form_for`
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
