7f5b8e7bef357bf1936d72bcdd9c196cdae634b6
Marco Gomes <marco6_1@hotmail.com>
2024-05-26 21:13:43 +0100

    watch list: add destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
 
-  resources :bookmarks, only: :destroy
+  resources :bookmarks, only: [:destroy]
 
   resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create]
